57b130c15f858593eab22746bfa60842e4ba6b8c
Shubiksha <shubiksha.b@finestcoder.com>
2025-04-03 21:52:22 +0530

    added signals to profile

--- py_ShowMe/userProfile/apps.py
+++ py_ShowMe/userProfile/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UserprofileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userProfile'
+
+    def ready(self):
+        import userProfile.signals  # Ensure signals are loaded
