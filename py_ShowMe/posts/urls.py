68baf6e279a1089bf1fb57f7c6eac29889b48eab
Shubiksha <shubiksha.b@finestcoder.com>
2025-04-04 11:13:56 +0530

    add the delete post functionality

--- py_ShowMe/posts/urls.py
+++ py_ShowMe/posts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 app_name = 'posts'
 
-from .views import PostListCreateView
+from .views import PostListCreateView,PostDetailView
 
 urlpatterns = [
     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
