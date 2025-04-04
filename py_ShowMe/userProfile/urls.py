aab0f9a2036f33398e59fa08f866a841115665d0
Shubiksha <shubiksha.b@finestcoder.com>
2025-04-04 12:12:38 +0530

    Profile edit feature added

--- py_ShowMe/userProfile/urls.py
+++ py_ShowMe/userProfile/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView
+from .views import RegisterView, LoginView, LogoutView, ProfileUpdateView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='api_login'),
     path('logout/', LogoutView.as_view(), name='api_logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+     path('profile/edit/', ProfileUpdateView.as_view(), name='edit-profile'),
 ]
