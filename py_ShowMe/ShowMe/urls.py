110242d18528e8de914450f8f5de465bc60195fb
Shubiksha <shubiksha.b@finestcoder.com>
2025-04-09 18:30:38 +0530

    Updated urls.py

--- py_ShowMe/ShowMe/urls.py
+++ py_ShowMe/ShowMe/urls.py
@@ -9,7 +9,5 @@ urlpatterns = [
     path('userProfile/', include('userProfile.urls')),
     path('posts/', include('posts.urls', namespace='posts')),
     path('friends/', include('friends.urls', namespace='friends')),
-    path('auth/', include('dj_rest_auth.urls')),  # login/logout
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),  # registration
     path('auth/', include('allauth.socialaccount.urls')),  # social login
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
