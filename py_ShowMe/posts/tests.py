a78714f3dff97c68feb7f69cd4f0ea7d26679a68
Shubiksha <shubiksha.b@finestcoder.com>
2025-04-09 12:01:20 +0530

    Added test file and ERD Diagram to the code base

--- py_ShowMe/posts/tests.py
+++ py_ShowMe/posts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+# userProfile/tests.py
+from django.test import TestCase
+from django.apps import apps
+
+class PrintDBTablesTest(TestCase):
+    def test_print_tables(self):
+        for model in apps.get_models():
+            print(model._meta.db_table)
